6a177ce9a8b47a0b601249e0830bd147caa41cdc
Yuri Schimke <yuri@schimke.ee>
2021-02-27 16:20:45 +0000

    app engine

--- src/main/kotlin/io/rsocket/demo/RsocketDemoApplication.kt
+++ src/main/kotlin/io/rsocket/demo/RsocketDemoApplication.kt
@@ -1,10 +1,28 @@
 package io.rsocket.demo
 
+import io.rsocket.core.Resume
+import io.rsocket.resume.InMemoryResumableFramesStore
+import org.apache.commons.logging.LogFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.rsocket.server.RSocketServerCustomizer
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import java.time.Duration
 
 @SpringBootApplication
-class RsocketDemoApplication
+class RsocketDemoApplication {
+	private val logger = LogFactory.getLog(RsocketDemoApplication::class.java)
+
+	@Bean
+	fun rSocketResume(): RSocketServerCustomizer = RSocketServerCustomizer {
+		logger.info("Configuring resume for 15 minutes")
+		it.resume(Resume()
+			.sessionDuration(Duration.ofMinutes(15))
+			.cleanupStoreOnKeepAlive()
+			.storeFactory { InMemoryResumableFramesStore("server", 50_000) }
+		)
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<RsocketDemoApplication>(*args)
